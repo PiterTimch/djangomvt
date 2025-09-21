60ee2f8528b00793b4fc0560fc9fc08e32af85a7
Petro <petrotymchuck@gmail.com>
2025-09-21 07:55:53 +0300

    is good work with categories

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+
 # Create your models here.
